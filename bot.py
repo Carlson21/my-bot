c49e83b338e92af9820ea1ac25b1b073e7c0190a
Carlson21 <45288359+Carlson21@users.noreply.github.com>
2018-12-07 14:57:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,10 @@ import csv
 import pandas as pd
 import string
 import random
-from flask import Flask, request
-import os
+
 
 bot = telebot.TeleBot('624431408:AAEMRUTdMwx_xJg7EFemgFn-wdrAQ0hb6tc')
-TOKEN = '624431408:AAEMRUTdMwx_xJg7EFemgFn-wdrAQ0hb6tc'
+
 
 
 titles = []
@@ -21,8 +20,6 @@ full_prices = []
 ratings = []
 sellerss = []
 links = []
-
-server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def command_c(message):
@@ -276,21 +273,8 @@ def command_c(message):
                     print(sys.exc_info())
 
 
-            @server.route('/' + TOKEN, methods=['POST'])
-            def getMessage():
-                bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-                return "!", 200
-
-            @server.route("/")
-            def webhook():
-                bot.remove_webhook()
-                bot.set_webhook(url='https://dashboard.heroku.com/apps/bot-amz-t' + TOKEN)
-                main()
-                return "!", 200
-
-
-
             if __name__ == '__main__':
-                server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+                main()
+
 
 
