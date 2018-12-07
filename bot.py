d4e123d3ec8b1f9c3dbcc2bdd2c485f0128346a2
Carlson21 <45288359+Carlson21@users.noreply.github.com>
2018-12-07 14:42:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,12 @@ import csv
 import pandas as pd
 import string
 import random
-from telegram.ext.dispatcher import run_async
+from flask import Flask, request
+import os
 
 bot = telebot.TeleBot('624431408:AAEMRUTdMwx_xJg7EFemgFn-wdrAQ0hb6tc')
+TOKEN = '624431408:AAEMRUTdMwx_xJg7EFemgFn-wdrAQ0hb6tc'
 
-@run_async
-def echo(bot, update):
-    bot.sendMessage(update.message.chat_id, text=update.message.text)
 
 titles = []
 brands = []
@@ -22,6 +21,8 @@ full_prices = []
 ratings = []
 sellerss = []
 links = []
+
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def command_c(message):
@@ -275,8 +276,21 @@ def command_c(message):
                     print(sys.exc_info())
 
 
-            if __name__ == '__main__':
+            @server.route('/' + TOKEN, methods=['POST'])
+            def getMessage():
+                bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+                return "!", 200
+
+            @server.route("/")
+            def webhook():
+                bot.remove_webhook()
+                bot.set_webhook(url='https://dashboard.heroku.com/apps/bot-amz-t' + TOKEN)
                 main()
+                return "!", 200
 
 
-bot.polling(none_stop=True, interval=0)
+
+            if __name__ == '__main__':
+                server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
